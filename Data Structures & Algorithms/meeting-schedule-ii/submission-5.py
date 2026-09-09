0c03e579fde7322f4384fddf400e955c8eec5e54
Xiao Xu <165375679+XiaoXu222@users.noreply.github.com>
2026-09-08 22:24:36 -0700

    Add: meeting-schedule-ii - submission-5

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-5.py
@@ -0,0 +1,28 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        timeLine = []
+        for i in range(len(intervals)):
+            timeLine.append([intervals[i].start, 1])
+            timeLine.append([intervals[i].end, -1])
+
+        # timeLine.sort()
+        timeLine.sort(key=lambda x: [x[0], x[1]])
+
+        currRooms = 0
+        res = 0
+        for j in range(len(timeLine)):
+            currRooms += timeLine[j][1]
+            res = max(res, currRooms)
+        return res
+        
+
+
+        
